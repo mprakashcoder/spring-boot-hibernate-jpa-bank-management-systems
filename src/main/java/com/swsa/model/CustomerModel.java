bb7a07c387fb91a827af6949daf18c26b3b58c25
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-17 10:02:38 +0530

    committed

---
+++ src/main/java/com/swsa/model/CustomerModel.java
@@ -0,0 +1,13 @@
+package com.swsa.model;
+import lombok.Data;
+
+@Data
+public class CustomerModel {
+
+   private   int customerId;
+    private  String name;
+    private   long mobileNo;
+    private  String address;
+    private  String emailId;
+    private long aadhaarNo;
+}
