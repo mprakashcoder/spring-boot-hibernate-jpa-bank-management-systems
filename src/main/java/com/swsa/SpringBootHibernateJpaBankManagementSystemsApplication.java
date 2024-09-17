e3c0399dce2eb7bb6b1c034cabed9faa62596259
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-17 10:05:13 +0530

    committed

---
+++ src/main/java/com/swsa/SpringBootHibernateJpaBankManagementSystemsApplication.java
@@ -0,0 +1,14 @@
+package com.swsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootHibernateJpaBankManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateJpaBankManagementSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swsa/SpringBootHibernateJpaBankManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateJpaBankManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
