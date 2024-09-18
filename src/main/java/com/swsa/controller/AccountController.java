b2cc3537d7a8b9182bcd5f6641bb7d3efa964692
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-18 16:38:43 +0530

    committed

---
+++ src/main/java/com/swsa/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.swsa.controller;
+import com.swsa.model.Accountmodel;
+import com.swsa.service.AccountService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping(value = "/accountManagement")
+public class AccountController {
+
+    @Resource(name = "accountService")
+    private AccountService accountService;
+
+    @GetMapping("/accounts")
+    public List<Accountmodel> geAccounts(){
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/account/{id}")
+    public Accountmodel getAccount(@PathVariable Long id){
+        return accountService.getAccountById(String.valueOf(id));
+    }
+
+    @PostMapping("/account")
+    public Accountmodel saveAccount(final @RequestBody Accountmodel accountmodel){
+        return accountService.saveAccount(accountmodel);
+    }
+
+}
