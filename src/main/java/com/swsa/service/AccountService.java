eae52910a08c8cf1c39674ba3b1d125f1b56474e
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-18 16:45:54 +0530

    committed

---
+++ src/main/java/com/swsa/service/AccountService.java
@@ -0,0 +1,8 @@
+package com.swsa.service;
+import com.swsa.model.Accountmodel;
+import java.util.List;
+public interface AccountService {
+    Accountmodel saveAccount(Accountmodel accountmodel);
+    List<Accountmodel> getAllAccounts();
+    Accountmodel getAccountById(final String accountNumber);
+}
