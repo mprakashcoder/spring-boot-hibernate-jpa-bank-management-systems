5ba8c1d886fe84737bbecbf7b5106bc1aabf2128
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-23 14:44:51 +0530

    committed

--- src/main/java/com/swsa/SpringBootHibernateJpaBankManagementSystemsApplication.java
+++ src/main/java/com/swsa/SpringBootHibernateJpaBankManagementSystemsApplication.java
@@ -2,9 +2,10 @@ package com.swsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-
+@EnableJpaAuditing
 public class SpringBootHibernateJpaBankManagementSystemsApplication {
 
 	public static void main(String[] args) {
