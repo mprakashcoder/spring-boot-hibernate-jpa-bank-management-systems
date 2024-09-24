e9bb1e6a8395f818f4e51c77328d88f4d2dbd6fd
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-24 20:04:55 +0530

    committed

--- src/main/java/com/swsa/service/AccountService.java
+++ src/main/java/com/swsa/service/AccountService.java
@@ -1,8 +1,15 @@
 package com.swsa.service;
+import com.swsa.domain.Account;
 import com.swsa.model.Accountmodel;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 public interface AccountService {
     Accountmodel saveAccount(Accountmodel accountmodel);
+
+    Account deposit(Long id, Double balance) throws Exception;
+
     List<Accountmodel> getAllAccounts();
     Accountmodel getAccountById(final Long accountNumber);
+    Account  getAccount(Long id);
 }
