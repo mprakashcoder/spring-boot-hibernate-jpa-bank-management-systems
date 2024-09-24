0a665bc6707df8d2f4467d37002772f162caecb7
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-24 20:03:15 +0530

    committed

--- src/main/java/com/swsa/controller/AccountController.java
+++ src/main/java/com/swsa/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.swsa.controller;
 import com.swsa.model.Accountmodel;
 import com.swsa.service.AccountService;
 import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
@@ -20,10 +21,30 @@ public class AccountController {
     public Accountmodel getAccount(@PathVariable Long id){
         return accountService.getAccountById(id);
     }
+//
+//    @PostMapping("/account")
+//    public Accountmodel saveAccount(final @RequestBody Accountmodel accountmodel){
+//        return accountService.saveAccount(accountmodel);
+//
+//    }
 
     @PostMapping("/account")
-    public Accountmodel saveAccount(final @RequestBody Accountmodel accountmodel){
-        return accountService.saveAccount(accountmodel);
+    public ResponseEntity<Accountmodel> saveAccount(@RequestBody Accountmodel account) {
+
+        try {
+            return ResponseEntity.ok(accountService.saveAccount(account));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);  // Handle errors and return bad request if needed
+        }
+    }
+
+    @PutMapping("/deposit/{id}")
+    public ResponseEntity<Accountmodel> deposit(@PathVariable Long id, @RequestBody  Double balance) {
+        try {
+            return ResponseEntity.ok(accountService.deposit(id, balance));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
 }
